61d94af253592b3cf07986847688559c08d70733
André Gabriel <andreon098@gmail.com>
2025-05-17 10:57:53 -0300

    feat: register extensions in create_app function

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 
 from .config import config, EnvironmentType
+from .extensions import register_extensions
 
 
 def create_app(config_name: EnvironmentType = 'development') -> Flask:
     app = Flask(__name__)
     app.config.from_object(config[config_name])
+
+    register_extensions(app)
 
     return app
