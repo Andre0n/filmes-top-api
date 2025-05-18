0a3214e61e0ea951768820638218c30debcb750d
André Gabriel <andreon098@gmail.com>
2025-05-18 09:30:14 -0300

    feat: import all models in the app initialization

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask import Flask
 from .config import config, EnvironmentType
 from .extensions import register_extensions
 
+from .models import *
+
 
 def create_app(config_name: EnvironmentType = 'development') -> Flask:
     app = Flask(__name__)
