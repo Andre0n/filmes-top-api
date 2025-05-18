8f547f834046c777bf5653c212055c22494a16b4
André Gabriel <andreon098@gmail.com>
2025-05-18 14:07:36 -0300

    feat: implement MovieController with GET and POST methods

--- app/controllers/movie_controller.py
+++ app/controllers/movie_controller.py
@@ -1,17 +1,24 @@
+from flask import Response
 from flask.views import MethodView
+
+from ..repositories import MovieRepository
+from ..schemas.movie.create_movie_schema import CreateMovieRequest
+from ..schemas.movie.get_movie_request_schema import GetMovieRequest
+from ..services.movie_service import MovieService
+from ..validators.body_validator import validate_body
+from ..validators.query_validator import validate_query
 
 
 class MovieController(MethodView):
-    def get(self, movie_id: str|None = None):
+    def __init__(self) -> None:
+        self.movie_service = MovieService(movie_repository=MovieRepository())
+
+    @validate_query(GetMovieRequest)
+    def get(self, movie_id: str, query: GetMovieRequest) -> Response:
         if movie_id:
-            return f'MovieController GET method with movie_id: {movie_id}'
-        return 'MovieController GET method'
+            return self.movie_service.get_movie(movie_id)
+        return self.movie_service.get_movies(query)
 
-    def post(self):
-        return 'MovieController POST method'
-
-    def put(self):
-        return 'MovieController PUT method'
-
-    def delete(self):
-        return 'MovieController DELETE method'
+    @validate_body(CreateMovieRequest, 'create_movie_request')
+    def post(self, create_movie_request: CreateMovieRequest) -> Response:
+        return self.movie_service.create_movie(create_movie_request)
