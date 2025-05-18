78c05a15cddbea41e23273a27aca3fc7b1b363fa
André Gabriel <andreon098@gmail.com>
2025-05-18 16:10:04 -0300

    feat: implement ApiResponse class for standardized API responses

---
+++ app/utils/api_response.py
@@ -0,0 +1,17 @@
+from typing import Any
+from flask import Response, make_response
+
+
+class ApiResponse:
+    @staticmethod
+    def send(
+        status_code: int = 200, message: str = '', data: Any = None
+    ) -> Response:
+        return make_response(
+            {
+                'code': status_code,
+                'message': message,
+                'data': data,
+            },
+            status_code,
+        )
