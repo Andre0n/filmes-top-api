cab3025446fefd528ffcb4583843b56d88a09965
André Gabriel <andreon098@gmail.com>
2025-05-18 16:10:11 -0300

    feat: replace jsonify with ApiResponse for consistent responses

--- app/services/movie_service.py
+++ app/services/movie_service.py
@@ -1,9 +1,10 @@
-from flask import Response, jsonify
+from flask import Response
 
 from ..dtos.movie_dto import ListMovieResponseDto, MovieResponseDto
 from ..repositories.movies_repository import MovieRepository
 from ..schemas.movie.create_movie_schema import CreateMovieRequest
 from ..schemas.movie.get_movie_request_schema import GetMovieRequest
+from ..utils.api_response import ApiResponse
 
 
 class MovieService:
@@ -15,16 +16,25 @@ class MovieService:
             movies = self.movie_repository.find_by_title(
                 get_movie_request.search
             )
-            return jsonify(ListMovieResponseDto.from_model(movies).__dict__)
+            return ApiResponse.send(
+                status_code=200,
+                data=ListMovieResponseDto.from_model(movies).__dict__,
+            )
 
+        page = get_movie_request.page or 1
+        page_size = get_movie_request.limit or 100
         movies = self.movie_repository.find_all(
-            get_movie_request.page, get_movie_request.limit
+            (page - 1) * page_size, page_size
         )
-        return jsonify(ListMovieResponseDto.from_model(movies).__dict__)
+        return ApiResponse.send(
+            data=ListMovieResponseDto.from_model(movies).__dict__
+        )
 
     def get_movie(self, movie_id: str) -> Response:
         movie = self.movie_repository.find_by_id(movie_id)
-        return jsonify(MovieResponseDto.from_model(movie).__dict__)
+        return ApiResponse.send(
+            data=MovieResponseDto.from_model(movie).__dict__
+        )
 
     def create_movie(
         self, create_movie_request: CreateMovieRequest
@@ -32,4 +42,8 @@ class MovieService:
         movie = self.movie_repository.create(
             title=create_movie_request.title, year=create_movie_request.year
         )
-        return jsonify(MovieResponseDto.from_model(movie).__dict__)
+        return ApiResponse.send(
+            status_code=201,
+            message='Filme criado com sucesso',
+            data=MovieResponseDto.from_model(movie).__dict__,
+        )
