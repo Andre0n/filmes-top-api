e6772786133e9e6ae8b5db6688d036d4213e5af4
André Gabriel <andreon098@gmail.com>
2025-05-18 09:48:20 -0300

    feat: enhance GET method to support movie_id parameter

--- app/controllers/movie_controller.py
+++ app/controllers/movie_controller.py
@@ -2,7 +2,9 @@ from flask.views import MethodView
 
 
 class MovieController(MethodView):
-    def get(self):
+    def get(self, movie_id: str|None = None):
+        if movie_id:
+            return f'MovieController GET method with movie_id: {movie_id}'
         return 'MovieController GET method'
 
     def post(self):
