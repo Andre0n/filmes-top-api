d6f5e8449286483be45a266d8e69e696b80ab8a6
André Gabriel <andreon098@gmail.com>
2025-05-18 09:48:35 -0300

    feat: register api_blueprint in the application factory

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
+from .blueprints.api_blueprint import api_blueprint
 from .config import EnvironmentType, config
 from .extensions import register_extensions
 from .models import *
@@ -10,5 +11,7 @@ def create_app(config_name: EnvironmentType = 'development') -> Flask:
     app.config.from_object(config[config_name])
 
     register_extensions(app)
+
+    app.register_blueprint(api_blueprint)
 
     return app
