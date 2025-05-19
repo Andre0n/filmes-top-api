6311b2601197da43796e9c48c57f922431125d8f
André Gabriel <andreon098@gmail.com>
2025-05-19 14:55:26 -0300

    refactor: optimize app initialization in main.py

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from os import environ
+
 from app import create_app
 
+app = create_app(environ.get('FLASK_ENV') or 'development')
+
 if __name__ == '__main__':
-    create_app().run()
+    app.run()
