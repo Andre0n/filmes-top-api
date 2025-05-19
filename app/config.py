e6d8a77e18c74be871fc8bc029670e7a0a97c509
André Gabriel <andreon098@gmail.com>
2025-05-19 01:48:37 -0300

    feat: add JWT configuration for access and refresh token expiration

--- app/config.py
+++ app/config.py
@@ -10,6 +10,9 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     API_TITLE = 'FilmesTop API'
     API_VERSION = 'v1'
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    JWT_ACCESS_TOKEN_EXPIRES = 3600  # 1 hour
+    JWT_REFRESH_TOKEN_EXPIRES = 604800  # 7 days
 
 
 class DevelopmentConfig(Config):
