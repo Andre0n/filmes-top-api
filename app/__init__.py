dfcd23e4523febd60ddfea31df0f844c1815af1d
André Gabriel <andreon098@gmail.com>
2025-05-19 16:23:37 -0300

    feat: initialize Swagger for API documentation setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
+from flasgger import Swagger  # pyright: ignore
 from flask import Flask
 
 from .blueprints.api_blueprint import api_blueprint
 from .config import config
 from .extensions import register_extensions
 from .models import *
+from .utils.api_docs import init_swagger
 from .utils.error_handlers import handle_exception
 
 
@@ -12,6 +14,7 @@ def create_app(config_name: str = 'development') -> Flask:
     app.config.from_object(config[config_name])
 
     register_extensions(app)
+    init_swagger(app)
 
     app.register_blueprint(api_blueprint)
     app.register_error_handler(Exception, handle_exception)
