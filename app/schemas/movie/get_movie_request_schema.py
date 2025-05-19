7924a875ac7f5c063686c5b6e903509ba22fc203
André Gabriel <andreon098@gmail.com>
2025-05-19 02:55:37 -0300

    feat: add genre field to GetMovieRequest schema

--- app/schemas/movie/get_movie_request_schema.py
+++ app/schemas/movie/get_movie_request_schema.py
@@ -7,3 +7,4 @@ class GetMovieRequest(BaseModel):
     page: Optional[int] = Field(default=1, ge=1)
     limit: Optional[int] = Field(default=100, ge=1, le=100)
     search: Optional[str] = Field(default=None, max_length=100)
+    genre: Optional[str] = Field(default=None, max_length=50)
