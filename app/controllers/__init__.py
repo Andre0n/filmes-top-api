5982bdadb6b81608c829da3cfdb6fa4aa829fe07
André Gabriel <andreon098@gmail.com>
2025-05-19 02:49:43 -0300

    feat: include RegisterController and LoginController in exports

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,3 +1,4 @@
 from .movie_controller import MovieController
+from .auth_controller import RegisterController, LoginController
 
-__all__ = ['MovieController']
+__all__ = ['MovieController', 'RegisterController', 'LoginController']
