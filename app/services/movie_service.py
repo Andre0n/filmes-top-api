018fc027322493ed568b83b8cd8b056a4670534d
André Gabriel <andreon098@gmail.com>
2025-05-19 11:48:18 -0300

    feat: implement add_review method for movie reviews handling

--- app/services/movie_service.py
+++ app/services/movie_service.py
@@ -2,16 +2,16 @@ from datetime import datetime, timedelta
 
 from flask import Response
 
-from app.types.error import ApiErrorCodes
-from app.utils.exceptions import CustomException
-
-from ..dtos.movie_dto import ListMovieResponseDto, MovieResponseDto
-from ..repositories.movies_repository import MovieRepository
-from ..repositories.rental_repository import RentalRepository
-from ..repositories.user_repository import UserRepository
+from ..dtos.movie_dto import ListMovieResponseDto, ListRentedMoviesResponseDto, MovieResponseDto
+from ..dtos.review_dto import ReviewDto
+from ..repositories import MovieRepository, RentalRepository, UserRepository
+from ..repositories.review_repository import ReviewRepository
+from ..schemas.movie.add_review_schema import AddReviewRequest
 from ..schemas.movie.create_movie_schema import CreateMovieRequest
 from ..schemas.movie.get_movie_request_schema import GetMovieRequest
+from ..types.error import ApiErrorCodes
 from ..utils.api_response import ApiResponse
+from ..utils.exceptions import CustomException
 
 
 class MovieService:
@@ -20,21 +20,21 @@ class MovieService:
         movie_repository: MovieRepository,
         user_repository: UserRepository,
         rental_repository: RentalRepository,
+        review_repository: ReviewRepository,
     ):
         self.movie_repository = movie_repository
         self.user_repository = user_repository
         self.rental_repository = rental_repository
+        self.review_repository = review_repository
 
-    def get_movies(
-        self, get_movie_request: GetMovieRequest, user_id: str = ''
-    ) -> Response:
+    def get_movies(self, get_movie_request: GetMovieRequest) -> Response:
         if get_movie_request.search:
             movies = self.movie_repository.find_by_title(
                 get_movie_request.search
             )
             return ApiResponse.send(
                 status_code=200,
-                data=ListMovieResponseDto.from_model(movies).__dict__,
+                data={'movies': ListMovieResponseDto.from_model(movies).__dict__},
             )
 
         if get_movie_request.genre:
@@ -43,7 +43,7 @@ class MovieService:
             )
             return ApiResponse.send(
                 status_code=200,
-                data=ListMovieResponseDto.from_model(movies).__dict__,
+                data={'movies': ListMovieResponseDto.from_model(movies).__dict__},
             )
 
         page = get_movie_request.page or 1
@@ -52,7 +52,7 @@ class MovieService:
             (page - 1) * page_size, page_size
         )
         return ApiResponse.send(
-            data=ListMovieResponseDto.from_model(movies).__dict__
+            data={'movies': ListMovieResponseDto.from_model(movies).__dict__}
         )
 
     def get_movie(self, movie_id: str) -> Response:
@@ -98,7 +98,6 @@ class MovieService:
         )
 
         return ApiResponse.send(
-            status_code=200,
             message='Filme alugado com sucesso',
         )
 
@@ -109,20 +108,57 @@ class MovieService:
             raise CustomException(ApiErrorCodes.USER_NOT_FOUND)
 
         rentals = self.rental_repository.find_by_user_id(user_id)
-        if not rentals:
-            return ApiResponse.send(
-                status_code=200,
-                message='Nenhum filme alugado encontrado',
-                data=[],
-            )
-
-        rented_movies = [
-            MovieResponseDto.from_model(rental.movie).__dict__
-            for rental in rentals
-        ]
 
         return ApiResponse.send(
-            status_code=200,
             message='Filmes alugados encontrados com sucesso',
-            data=rented_movies,
+            data={'movies': ListRentedMoviesResponseDto.from_model(rentals).__dict__},
+        )
+
+    def add_review(
+        self, movie_id: str, user_id: str, add_review_request: AddReviewRequest
+    ) -> Response:
+        movie = self.movie_repository.find_by_id(movie_id)
+        user = self.user_repository.find_by_id(user_id)
+
+        if not user:
+            raise CustomException(ApiErrorCodes.USER_NOT_FOUND)
+        if not movie:
+            raise CustomException(ApiErrorCodes.MOVIE_NOT_FOUND)
+
+        rental = self.rental_repository.find_by_user_and_movie(
+            user_id=user_id, movie_id=movie_id
+        )
+
+        if not rental:
+            raise CustomException(
+                ApiErrorCodes.TRY_REVIEW_NOT_RENTED_MOVIE,
+                data={
+                    'movie_id': movie_id,
+                },
+            )
+
+        review = self.review_repository.find_by_user_and_movie(
+            user_id=user_id, movie_id=movie_id
+        )
+
+        if review:
+            raise CustomException(
+                ApiErrorCodes.TRY_REVIEW_ALREADY_RATED_MOVIE,
+                data={
+                    'movie_id': movie_id,
+                },
+            )
+
+        review = self.review_repository.create(
+            user_id=user_id,
+            movie_id=movie_id,
+            rating=add_review_request.rating,
+            comment=add_review_request.comment,
+        )
+
+        self.movie_repository.update_rating(movie, review.rating)
+
+        return ApiResponse.send(
+            message='Avaliação criada com sucesso',
+            data=ReviewDto.from_model(review).__dict__,
         )
