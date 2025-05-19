12748802e03e44073976684cdee2b630bc58e9a7
André Gabriel <andreon098@gmail.com>
2025-05-19 02:27:34 -0300

    style: add pyright ignore comments for jwt_required decorator

--- app/controllers/movie_controller.py
+++ app/controllers/movie_controller.py
@@ -1,6 +1,6 @@
 from flask import Response
 from flask.views import MethodView
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required  # pyright: ignore
 
 from ..repositories import MovieRepository
 from ..schemas.movie.create_movie_schema import CreateMovieRequest
@@ -15,13 +15,13 @@ class MovieController(MethodView):
         self.movie_service = MovieService(movie_repository=MovieRepository())
 
     @validate_query(GetMovieRequest)
-    @jwt_required()
+    @jwt_required()   # type: ignore
     def get(self, movie_id: str, query: GetMovieRequest) -> Response:
         if movie_id:
             return self.movie_service.get_movie(movie_id)
         return self.movie_service.get_movies(query)
 
     @validate_body(CreateMovieRequest, 'create_movie_request')
-    @jwt_required()
+    @jwt_required()   # type: ignore
     def post(self, create_movie_request: CreateMovieRequest) -> Response:
         return self.movie_service.create_movie(create_movie_request)
