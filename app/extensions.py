2c074a56fbd2c7b3048b7bb456adb09961b9a100
André Gabriel <andreon098@gmail.com>
2025-05-19 01:48:46 -0300

    feat: initialize JWTManager in extensions for authentication

--- app/extensions.py
+++ app/extensions.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 migrate = Migrate()
+jwt = JWTManager()
 
 
 def register_extensions(app: Flask) -> None:
@@ -12,3 +14,4 @@ def register_extensions(app: Flask) -> None:
     """
     db.init_app(app)
     migrate.init_app(app, db)
+    jwt.init_app(app)
